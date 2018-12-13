d393adc003cce53c08d869faae89c34a82ecde27
yosuke25 <yosuke25@MacBook-Pro--yosuke25.local>
2018-12-13 10:59:50 +0900

    first commit

---
+++ Assets/NiconicoTVChanController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NiconicoTVChanController : MonoBehaviour {
+
+  public GameObject leftButton;
+  public GameObject rightButton;
+  OnTouch leftTouch;
+  OnTouch rightTouch;
+
+  void Start () {
+    leftTouch = leftButton.GetComponent<OnTouch>();
+    rightTouch = rightButton.GetComponent<OnTouch>();
+  }
+
+  void Update () {
+    if (leftTouch.flag && rightTouch.flag) {
+      return;
+    } else if (leftTouch.flag) {
+      Move(-0.1f);
+    } else if (rightTouch.flag) {
+      Move(0.1f);
+    }
+  }
+
+  void Move (float distance) {
+    Vector3 newPosition = this.gameObject.transform.position;
+    newPosition.x += distance;
+    this.gameObject.transform.position = newPosition;
+  }
+
+
+}
---
+++ Assets/OnTouch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class OnTouch : MonoBehaviour {
+
+  Dictionary<int, bool> downFinger = new Dictionary<int, bool>();
+
+  public bool flag = false;
+
+  void Update () {
+    if (Input.touchCount <= 0) {
+      flag = false;
+      downFinger = new Dictionary<int, bool>();
+      return;
+    }
+
+    for (int i = 0; i < Input.touchCount; i++) {
+      Touch touch = Input.GetTouch(i);
+      if (!IsDownFinger(touch.fingerId) && (touch.phase == TouchPhase.Began) && RayHit(touch.position)) {
+        flag = true;
+        downFinger[touch.fingerId] = true;
+      } else if (IsDownFinger(touch.fingerId) && (touch.phase == TouchPhase.Ended)) {
+        flag = false;
+        downFinger[touch.fingerId] = false;
+      }
+    }
+  }
+
+  bool IsDownFinger (int fingerId) {
+    return (downFinger.ContainsKey(fingerId) && downFinger[fingerId]);
+  }
+
+  bool RayHit (Vector2 touchPosition) {
+    Ray2D ray = new Ray2D(Camera.main.ScreenToWorldPoint(touchPosition), Camera.main.transform.forward);
+    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+    Debug.DrawRay(ray.origin, Camera.main.transform.forward * 1000.0f, Color.red);
+    return ((hit.collider != null) && (hit.collider.gameObject == this.gameObject));
+  }
+
+}
